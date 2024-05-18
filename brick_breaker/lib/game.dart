82bd26ad7eccd0cda95b0fd032cbdaa2d66c5fff
chans123123 <chanyounng6@gmail.com>
2024-05-18 11:41:48 +0900

    brick breaker

---
+++ brick_breaker/lib/game.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class BrickBreakerGame extends FlameGame{
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 173, 223, 247);
+  }
+}
---
+++ brick_breaker/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:brick_breaker/game.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp (
+    const GameWidget<BrickBreakerGame>.controlled(gameFactory: BrickBreakerGame.new)
+    );
+}
