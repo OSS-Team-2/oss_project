c26125bb31d6f83263f782917833effc38657b50
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-19 12:12:03 +0900

    Update brick.dart

--- brick_breaker/lib/brick.dart
+++ brick_breaker/lib/brick.dart
@@ -2,3 +2,10 @@ import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
+
+class Brick extends PositionComponent {
+  late final Vector2 gridSize;
+
+  Brick({required this.gridSize}) : super(size: gridSize);
+  
+  Paint paint = Paint()..color = Colors.red;
