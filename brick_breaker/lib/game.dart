38c2bb7f54cbf2fd971665fa5af7ba2007fccbec
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-19 12:10:35 +0900

    Update game.dart

--- brick_breaker/lib/game.dart
+++ brick_breaker/lib/game.dart
@@ -1,8 +1,23 @@
+import 'package:brick_breaker/brick.dart';
+import 'package:flame/camera.dart';
 import 'dart:ui';
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 
 class BrickBreakerGame extends FlameGame{
-  Color backgroundColor() {
-    return const Color.fromARGB(255, 173, 223, 247);
+  static double grid = 0.1;
+  static late Vector2 gridSize;
+  
+  final World world = World();
+  late   CameraComponent cameraComponent = CameraComponent(world: world)
+  ..viewfinder.anchor = Anchor.topLeft;
+
+  @override
+  void onLoad() async{
+    await Flame.device.setLandscapeLeftOnly();
+    await Future.delayed(const Duration(seconds: 1)); 
+  
+    gridSize = Vector2(size.x * grid, size.y * grid);
   }
 }
