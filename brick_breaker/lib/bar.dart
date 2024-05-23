00fd5adc2ad265952b4f9946a1ebb8920a456bf7
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-23 23:09:27 +0900

    Create bar.dart

---
+++ brick_breaker/lib/bar.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+import 'package:flame/components.dart';
+
+class Bar extends PositionComponent{
+  final Vector2 gridSize;
+  Bar({required this.gridSize}) : super(size: Vector2(gridSize.x*2, gridSize.y /2));
+
+}
