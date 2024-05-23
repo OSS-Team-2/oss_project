0bbdc6035b89039577d7b76e39383a6d14343616
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-23 23:11:44 +0900

    Update bar.dart

--- brick_breaker/lib/bar.dart
+++ brick_breaker/lib/bar.dart
@@ -5,4 +5,8 @@ class Bar extends PositionComponent{
   final Vector2 gridSize;
   Bar({required this.gridSize}) : super(size: Vector2(gridSize.x*2, gridSize.y /2));
 
+  @override
+  void render(Canvas, canvas) {
+  canvas.drawRRect(getRRect, paint);
+  }
 }
