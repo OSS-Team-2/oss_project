c3cf4fa72fa3dcea33ef361eaf9c35e1bfb7c4c6
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-23 23:16:06 +0900

    Update bar.dart

--- brick_breaker/lib/bar.dart
+++ brick_breaker/lib/bar.dart
@@ -1,10 +1,12 @@
 import 'dart:ui';
 import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 
 class Bar extends PositionComponent{
   final Vector2 gridSize;
   Bar({required this.gridSize}) : super(size: Vector2(gridSize.x*2, gridSize.y /2));
-
+  Paint paint = Paint()..color = Colors.green;
+  
   @override
   void render(Canvas, canvas) {
   canvas.drawRRect(getRRect, paint);
