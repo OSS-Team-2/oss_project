3c5ea3d3f6a27277b750a4c1a3560087ff76480f
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-24 23:15:54 +0900

    Update game.dart

--- brick_breaker/lib/game.dart
+++ brick_breaker/lib/game.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
+import 'package:brick_breaker/bar.dart';
 
 class BrickBreakerGame extends FlameGame{
   static double grid = 0.1;
@@ -28,6 +29,8 @@ class BrickBreakerGame extends FlameGame{
   @override
   void onMount() {
     populateBricks(4, 8);
+    bar = Bar(gridSize: gridSize)..position = Vector2(size.x / 2 -  gridSize.x, size.y - gridSize.y);
+    world.addAll(bar);
     world.addAll(bricks);
     addAll([cameraComponent, world]);
   }
@@ -46,7 +49,7 @@ class BrickBreakerGame extends FlameGame{
     return const Color.fromARGB(255, 173, 223, 247);
   }
 
-   void populateBricks(int rows, int columns){
+  void populateBricks(int rows, int columns){
     Vector2 margin = Vector2(gridSize.x *0.2, gridSize.y *0.1);
     double totalWidth  = columns *(gridSize.x + margin.x) - margin.x;
     double firstPosX = (size.x - totalWidth)/2;
