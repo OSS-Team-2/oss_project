fa114abf308d58e0143c2a525c555603bc9005e5
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-25 22:59:31 +0900

    Update bar.dart

--- brick_breaker/lib/bar.dart
+++ brick_breaker/lib/bar.dart
@@ -1,25 +1,37 @@
 import 'dart:ui';
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/events.dart';
 
-class Bar extends PositionComponent with DragCallbacks{
+class Bar extends PositionComponent with DragCallbacks {
   final Vector2 gridSize;
-  Bar({required this.gridSize}) : super(size: Vector2(gridSize.x*2, gridSize.y /2));
+
+  Bar({required this.gridSize})
+      : super(size: Vector2(gridSize.x * 2, gridSize.y / 2));
+
   Paint paint = Paint()..color = Colors.green;
-  
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    add(RectangleHitbox());
+  }
+
   @override
   void render(Canvas canvas) {
+    super.render(canvas);
     canvas.drawRRect(getRRect(), paint);
   }
-  
+
   RRect getRRect() {
     return RRect.fromRectAndRadius(size.toRect(), Radius.circular(gridSize.x * 0.1));
   }
-  
+
   @override
   void onDragUpdate(DragUpdateEvent event) {
     super.onDragUpdate(event);
-    x += event.localDelta.x;
+    position.add(event.localDelta); 
   }
 }
