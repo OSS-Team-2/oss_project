2ff37e81b785362a12dd478bd74edf85aa1f9032
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-29 22:40:14 +0900

    Update game.dart

--- brick_breaker/lib/game.dart
+++ brick_breaker/lib/game.dart
@@ -1,41 +1,40 @@
-import 'package:brick_breaker/brick.dart';
-import 'package:flame/camera.dart';
-import 'dart:ui';
 import 'dart:async';
-import 'package:flame/game.dart';
+import 'dart:ui';
+
+import 'package:brick_breaker/ball.dart';
+import 'package:brick_breaker/bar.dart';
+import 'package:brick_breaker/brick.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
-import 'package:brick_breaker/bar.dart';
-import 'package:brick_breaker/ball.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 
-class BrickBreakerGame extends FlameGame {
+class BrickBreakerGame  extends FlameGame with HasCollisionDetection{
   static double grid = 0.1;
   static late Vector2 gridSize;
-
-  final World world = World();
-  late CameraComponent cameraComponent = CameraComponent(world: world)
-    ..viewfinder.anchor = Anchor.topLeft;
-
+  final  World world = World();
+  late   CameraComponent cameraComponent = CameraComponent(world: world)
+  ..viewfinder.anchor = Anchor.topLeft;
+  int score = 0;
   List<Brick> bricks = [];
-  late Bar bar;
-  late Ball ball;
+
+  late final Bar bar;
+  late final Ball ball;
 
   @override
-  Future<void> onLoad() async {
-    await super.onLoad();
+  void onLoad() async{
     await Flame.device.setLandscapeLeftOnly();
     await Future.delayed(const Duration(seconds: 1));
 
-    gridSize = Vector2(size.x * grid, size.y * grid);
+    gridSize = Vector2(size.x * grid, size.y *grid);
   }
 
   @override
   void onMount() {
-    super.onMount();  
     populateBricks(4, 8);
-    bar = Bar(gridSize: gridSize)..position = Vector2(size.x / 2 - gridSize.x, size.y - gridSize.y);
+    bar = Bar(gridSize: gridSize)..position = Vector2(size.x/2 -  gridSize.x, size.y - gridSize.y);
     ball = Ball(gridSize: gridSize)..position = size / 2;
-    world.addAll([bar, ball]);
+    world.addAll([bar,ball]);
     world.addAll(bricks);
     addAll([cameraComponent, world]);
   }
@@ -48,24 +47,33 @@ class BrickBreakerGame extends FlameGame {
   @override
   void render(Canvas canvas) {
     super.render(canvas);
+    TextPainter tp = TextPainter(
+      text: TextSpan(
+        text: '점수: $score',
+        style: TextStyle(color: Colors.white, fontSize: 48),
+      ),
+      textDirection: TextDirection.ltr,
+    );
+    tp.layout();
+    tp.paint(canvas, Offset(0, size.y - tp.height));
   }
 
   @override
   Color backgroundColor() {
-    return const Color.fromARGB(255, 173, 223, 247);
+    return const Color.fromARGB(255, 163, 204, 163);
   }
 
-  void populateBricks(int rows, int columns) {
-    Vector2 margin = Vector2(gridSize.x * 0.2, gridSize.y * 0.1);
-    double totalWidth = columns * (gridSize.x + margin.x) - margin.x;
-    double firstPosX = (size.x - totalWidth) / 2;
+  void populateBricks(int rows, int columns){
+    Vector2 margin = Vector2(gridSize.x *0.2, gridSize.y *0.1);
+    double totalWidth  = columns *(gridSize.x + margin.x) - margin.x;
+    double firstPosX = (size.x - totalWidth)/2;  // points the center for brick
 
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < columns; j++) {
+    // Create Bricks
+    for(int i = 0; i<rows; i++){
+      for(int j =0; j<columns;j++){
         bricks.add(Brick(gridSize: gridSize)
-          ..position = Vector2(firstPosX + ((gridSize.x + margin.x) * j), margin.y + ((gridSize.y + margin.y) * i)));
+        ..position = Vector2(firstPosX + (gridSize.x+margin.x)*j, margin.y + (gridSize.y + margin.y)*i));
       }
     }
   }
 }
-
