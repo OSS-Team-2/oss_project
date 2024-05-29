0df40b2b1966dee5b6e53bdc1c72fa3da0a7e0a7
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-29 22:40:26 +0900

    Update brick.dart

--- brick_breaker/lib/brick.dart
+++ brick_breaker/lib/brick.dart
@@ -1,16 +1,15 @@
 import 'dart:ui';
 
+import 'package:brick_breaker/ball.dart';
 import 'package:brick_breaker/game.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flutter/material.dart';
 
 class Brick  extends PositionComponent with HasGameRef<BrickBreakerGame>, CollisionCallbacks{
   late final Vector2 gridSize;
 
   Brick({required this.gridSize}) : super(size: gridSize);
-
-  Paint paint = Paint()..color = Colors.red;
+  Paint paint = Paint()..color =  Color.fromARGB(255, 255, 88, 68);
 
   @override
   void onLoad() {
@@ -23,14 +22,16 @@ class Brick  extends PositionComponent with HasGameRef<BrickBreakerGame>, Collis
   }
 
   RRect getRRect() {
- 
     return RRect.fromRectAndRadius(size.toRect(),  Radius.circular(gridSize.x*0.1));
   }
 
-  @override
+ @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
     super.onCollision(intersectionPoints, other);
-    game.bricks.remove(this);
-    game.world.remove(this);
+    if (other is Ball) {
+      game.score++;
+      game.bricks.remove(this);
+      game.world.remove(this);
+    }
   }
 }
